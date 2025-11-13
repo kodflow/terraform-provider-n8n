192d5f64cbdc1a7397dc61db33369e48702baf02
Kodflow <133899878+kodflow@users.noreply.github.com>
2025-11-13 14:56:36 +0000

    fix: make copyright header script executable

---
+++ src/internal/provider/shared/copyright_test.go
@@ -0,0 +1,6 @@
+package shared
+
+// CopyrightTestFunction verifies copyright headers are added automatically.
+func CopyrightTestFunction() bool {
+	return true
+}
--- src/internal/provider/shared/test_hook.go
+++
@@ -1,6 +0,0 @@
-package shared
-
-// TestHookFunction is a test function to verify pre-commit hook functionality.
-func TestHookFunction() string {
-	return "testing copyright hook"
-}
